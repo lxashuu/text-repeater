e86130a89eb41f1ec11d020b44c776124583edba
Ashutosh <ashutoshjain1509@gmail.com>
2026-01-16 11:26:25 +0530

    text repeater.py

---
+++ text repeater.py
@@ -0,0 +1,8 @@
+''' text repeater by ashu '''
+print("enter  your word ")
+text = str(input())
+print ('how many times u wanna repeat', text )
+time = int(input())
+outcome = (text +"\n") * time
+print =("HERE IS YOUR \n" + outcome)
+
